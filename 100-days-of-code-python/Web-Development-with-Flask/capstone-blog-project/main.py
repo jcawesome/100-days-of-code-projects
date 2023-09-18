0489ccceac3c7e7bef272a8332f68d5f62010c61
jcawesome <pagtalunanjancarlo@hotmail.com>
2023-09-19 03:37:07 +0800

    capstone-project blog post v2

--- 100-days-of-code-python/Web-Development-with-Flask/capstone-blog-project/main.py
+++ 100-days-of-code-python/Web-Development-with-Flask/capstone-blog-project/main.py
@@ -1,28 +1,33 @@
 from flask import Flask, render_template
-from post import Post
 import requests
 
-posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
-post_objects = []
-for post in posts:
-    post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
-    post_objects.append(post_obj)
+blog_url = 'https://api.npoint.io/eb6cd8a5d783f501ee7d'
+blog_resp = requests.get(blog_url)
+posts = blog_resp.json()
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def get_all_posts():
-    return render_template("index.html", all_posts=post_objects)
+    return render_template("index.html", posts=posts)
 
 
-@app.route("/post/<int:index>")
-def show_post(index):
-    requested_post = None
-    for blog_post in post_objects:
-        if blog_post.id == index:
-            requested_post = blog_post
-    return render_template("post.html", post=requested_post)
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+
+@app.route('/post/<int:p_id>')
+def get_post(p_id):
+    post = posts[p_id - 1]
+    return render_template('post.html', post=post, image=post['image_url'])
 
 
 if __name__ == "__main__":
