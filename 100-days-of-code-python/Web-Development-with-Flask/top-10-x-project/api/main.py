6ff7a870ce7d76fa6109009f21a56ab065dc2523
jcawesome <pagtalunanjancarlo@hotmail.com>
2023-10-17 09:39:12 +0800

    Refactor to api folder and added MIT License

--- 100-days-of-code-python/Web-Development-with-Flask/top-10-x-project/api/main.py
+++ 100-days-of-code-python/Web-Development-with-Flask/top-10-x-project/api/main.py
@@ -5,9 +5,10 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import requests
+import os
 
+MOVIE_DB_API_KEY = os.environ.get('MOVIE_DB_API_KEY')
 MOVIE_DB_SEARCH_URL = "https://api.themoviedb.org/3/search/movie"
-MOVIE_DB_API_KEY = "ab30caad40ebb11d10baa5a4213c8359"
 MOVIE_DB_INFO_URL = "https://api.themoviedb.org/3/movie"
 MOVIE_DB_IMAGE_URL = "https://image.tmdb.org/t/p/w500"
 
